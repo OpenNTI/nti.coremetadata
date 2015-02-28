e72da58dfbaf425330e1622c6a9870b8799cd0c0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-28 18:03:50 +0000

    move mixin to core
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.dataserver.core/trunk@60516 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/core/mixins.py
+++ src/nti/dataserver/core/mixins.py
@@ -11,6 +11,13 @@ logger = __import__('logging').getLogger(__name__)
 
 import time
 
+from zope import interface
+from zope.container.contained import Contained
+
+from nti.schema.fieldproperty import UnicodeConvertingFieldProperty
+
+from .interfaces import IContained
+
 class CreatedTimeMixin(object):
 
 	createdTime = 0
@@ -20,3 +27,31 @@ class CreatedTimeMixin(object):
 		if self._SET_CREATED_MODTIME_ON_INIT and self.createdTime == 0:
 			self.createdTime = time.time()
 		super(CreatedTimeMixin,self).__init__( *args, **kwargs )
+
+@interface.implementer(IContained)
+class _ContainedMixin(Contained):
+	"""
+	Defines something that can be logically contained inside another unit
+	by reference. Two properties are defined, id and containerId.
+	"""
+
+	# It is safe to use these properties in persistent objects because
+	# they read/write to the __dict__ with the same name as the field,
+	# and setattr on the persistent object is what set _p_changed, so
+	# assigning to them still changes the object correctly
+	containerId = UnicodeConvertingFieldProperty(IContained['containerId'])
+	id = UnicodeConvertingFieldProperty(IContained['id'])
+
+	# __name__ is NOT automatically defined as an id alias, because that could lose
+	# access to existing data that has a __name__ in its instance dict
+
+	def __init__(self, *args, **kwargs ):
+		containerId = kwargs.pop( 'containerId', None )
+		containedId = kwargs.pop( 'containedId', None )
+		super(_ContainedMixin,self).__init__(*args, **kwargs)
+		if containerId is not None:
+			self.containerId = containerId
+		if containedId is not None:
+			self.id = containedId
+
+ContainedMixin = ZContainedMixin = _ContainedMixin
