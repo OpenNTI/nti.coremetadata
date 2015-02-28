c2a2d50ecdc5a073ea67d88b6c62ed4873d4edf3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-28 22:48:27 +0000

    move more classes to core
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.dataserver.core/trunk@60529 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dataserver/core/schema.py
+++ src/nti/dataserver/core/schema.py
@@ -10,6 +10,8 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from zope.schema.interfaces import InvalidURI
+from zope.schema.interfaces import ValidationError
+from zope.schema.fieldproperty import FieldProperty
 
 from nti.common import dataurl
 
@@ -34,3 +36,49 @@ class DataURI(_ValidURI):
 
 		super(DataURI, self).fromUnicode(value)
 		return dataurl.DataURL(value)
+
+class AbstractFieldProperty(FieldProperty):
+
+	def __init__( self, field, name=None ):
+		super(AbstractFieldProperty, self).__init__( field, name=name )
+		self._field = field
+
+	def _to_tuple(self, value):
+		if value and isinstance( value, list ):
+			value = tuple(value)
+		return value
+
+	def _adapt(self, value):
+		return self._field.fromObject( value )
+	
+	def __set__( self, inst, value ):
+		value = self._to_tuple(value)
+		try:
+			super(AbstractFieldProperty,self).__set__( inst, value )
+		except ValidationError:
+			## Hmm. try to adapt
+			value = self._adapt(value)
+			super(AbstractFieldProperty, self).__set__( inst, value )
+
+class BodyFieldProperty(AbstractFieldProperty):
+
+	def _adapt(self, value):
+		## allow ascii strings for old app tests
+		value = [x.decode('utf-8') if isinstance(x, str) else x for x in value]
+		value = tuple( (self._field.value_type.fromObject(x) for x in value ) )
+		return value
+NoteBodyFieldProperty = BodyFieldProperty
+
+class MessageInfoBodyFieldProperty(AbstractFieldProperty):
+
+	def _to_tuple(self, value):
+		# Turn bytes into text
+		if isinstance( value, str ):
+			value = value.decode( 'utf-8' )
+		# Wrap single strings automatically
+		if isinstance( value, unicode ):
+			value = (value,)
+		# Make immutable
+		if value and isinstance( value, list ):
+			value = tuple(value)
+		return value
