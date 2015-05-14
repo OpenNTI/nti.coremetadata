c087aa58de9e108289bf0ddd8fc1f04bbfb0feca
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-14 14:31:48 +0000

    more mixins closer to their interface
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.coremetadata/trunk@65282 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/coremetadata/mixins.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import time
+
+from zope import interface
+
+from .interfaces import ILastModified
+
+class CreatedTimeMixin(object):
+
+	createdTime = 0
+	_SET_CREATED_MODTIME_ON_INIT = True
+
+	def __init__(self, *args, **kwargs):
+		if self._SET_CREATED_MODTIME_ON_INIT and self.createdTime == 0:
+			self.createdTime = time.time()
+		super(CreatedTimeMixin,self).__init__( *args, **kwargs )
+
+class ModifiedTimeMixin(object):
+
+	lastModified = 0
+
+	def updateLastMod(self, t=None ):
+		self.lastModified = ( t if t is not None and t > self.lastModified else time.time() )
+		return self.lastModified
+
+	def updateLastModIfGreater( self, t ):
+		"Only if the given time is (not None and) greater than this object's is this object's time changed."
+		if t is not None and t > self.lastModified:
+			self.lastModified = t
+		return self.lastModified
+
+@interface.implementer(ILastModified)
+class CreatedAndModifiedTimeMixin(CreatedTimeMixin, ModifiedTimeMixin):
+
+	def __init__(self, *args, **kwargs):
+		# We set the times now so subclasses can rely on them
+		if self._SET_CREATED_MODTIME_ON_INIT:
+			self.createdTime = time.time()
+			self.updateLastModIfGreater(self.createdTime)
+		super(CreatedAndModifiedTimeMixin, self).__init__(*args, **kwargs)
