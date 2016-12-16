2d5936bff265b46240795767d24d71ccbcbae572
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-16 16:37:46 +0000

    start using nti.base
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.coremetadata/trunk@102765 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/coremetadata/interfaces.py
+++ src/nti/coremetadata/interfaces.py
@@ -14,11 +14,15 @@ from zope.interface.interfaces import ObjectEvent
 from zope.interface.interfaces import IObjectEvent
 
 from zope.lifecycleevent import ObjectModifiedEvent
+
 from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 
 from zope.schema import Iterable
 
 from zope.security.management import system_user
+
+from nti.base.interfaces import ICreated
+from nti.base.interfaces import ILastModified
 
 from nti.schema.field import Bool
 from nti.schema.field import Number
@@ -27,61 +31,18 @@ from nti.schema.field import ValidDatetime
 SYSTEM_USER_ID = system_user.id
 SYSTEM_USER_NAME = getattr(system_user, 'title').lower()
 
-class ICreatedTime(interface.Interface):
-	"""
-	Something that (immutably) tracks its created time.
-	"""
-
-	createdTime = Number(title=u"The timestamp at which this object was created.",
-						 description="Typically set automatically by the object.",
-						 default=0.0)
-
-class ILastModified(ICreatedTime):
-	"""
-	Something that tracks a modification timestamp.
-	"""
-
-	lastModified = Number(title=u"The timestamp at which this object or its contents was last modified.",
-						  default=0.0)
-
-class ILastViewed(ILastModified):
-	"""
-	In addition to tracking modification and creation times, this
-	object tracks viewing (or access) times.
-
-	For security sensitive objects, this may be set automatically in
-	an audit-log type fashion. The most typical use, however, will be
-	to allow clients to track whether or not the item has been
-	displayed to the end user since its last modification; in that
-	case, the client will be responsible for updating the value seen
-	here explicitly (we can not assume that requesting an object for
-	externalization, for example, results in viewing).
-
-	In some cases it may be necessary to supplement this object with
-	additional information such as a counter to get the desired
-	behaviour.
-	"""
-	# There is no zope.dublincore analoge for this.
-	lastViewed = Number(title="The timestamp at which this object was last viewed.",
-						default=0.0)
+import zope.deferredimport
+zope.deferredimport.initialize()
+zope.deferredimport.deprecated(
+	"Import from nti.base.interfaces instead",
+	ITitled='nti.base.interfaces:ITitled',
+	ILastViewed='nti.base.interfaces:ILastViewed',
+	ICreatedTime='nti.base.interfaces:ICreatedTime',)
 
 class IContentTypeMarker(interface.Interface):
 	"""
 	Marker interface for deriving mimetypes from class names.
 	"""
-
-class ICreated(interface.Interface):
-	"""
-	Something created by an identified entity.
-	"""
-	creator = interface.Attribute("The creator of this object.")
-
-class ITitled(interface.Interface):
-	"""
-	A piece of content with a title, either human created or potentially
-	automatically generated. (This differs from, say, a person's honorrific title.
-	"""
-	title = interface.Attribute("The title of this object.")
 
 class IRecordable(interface.Interface):
 	"""
