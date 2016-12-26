d62605a1523ea94e9ebb05d6d5251e45d88c5813
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-26 17:40:07 +0000

    moved items to nti.coremetata
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.coremetadata/trunk@103260 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/coremetadata/schema.py
+++ src/nti/coremetadata/schema.py
@@ -9,7 +9,10 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope.schema.fieldproperty import FieldProperty
+
 from zope.schema.interfaces import InvalidURI
+from zope.schema.interfaces import ValidationError
 
 from nti.base.interfaces import INamed
 
@@ -34,6 +37,52 @@ class DataURI(_ValidURI, ProDataURI): # order matters
 			self._reraise_validation_error(InvalidURI(value),
 										   value,
 										   _raise=True)
+
+class AbstractFieldProperty(FieldProperty):
+
+	def __init__(self, field, name=None):
+		super(AbstractFieldProperty, self).__init__(field, name=name)
+		self._field = field
+
+	def _to_tuple(self, value):
+		if value and isinstance(value, (set, list)):
+			value = tuple(value)
+		return value
+
+	def _adapt(self, value):
+		return self._field.fromObject(value)
+
+	def __set__(self, inst, value):
+		value = self._to_tuple(value)
+		try:
+			super(AbstractFieldProperty, self).__set__(inst, value)
+		except ValidationError:
+			# Hmm. try to adapt
+			value = self._adapt(value)
+			super(AbstractFieldProperty, self).__set__(inst, value)
+
+class BodyFieldProperty(AbstractFieldProperty):
+
+	def _adapt(self, value):
+		# Allow ascii strings for old app tests
+		value = [x.decode('utf-8') if isinstance(x, str) else x for x in value]
+		value = tuple((self._field.value_type.fromObject(x) for x in value))
+		return value
+NoteBodyFieldProperty = BodyFieldProperty
+
+class MessageInfoBodyFieldProperty(AbstractFieldProperty):
+
+	def _to_tuple(self, value):
+		# Turn bytes into text
+		if isinstance(value, str):
+			value = value.decode('utf-8')
+		# Wrap single strings automatically
+		if isinstance(value, unicode):
+			value = (value,)
+		# Make immutable
+		if value and isinstance(value, (set, list)):
+			value = tuple(value)
+		return value
 
 def legacyModeledContentBodyTypes():
 	return [SanitizedHTMLContentFragment(min_length=1,
