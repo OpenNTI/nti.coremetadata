9c5e33a0fcabdf0f006c39081ec97ff7532d4926
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-13 00:14:46 +0000

    move Threadable interfaces to its proper module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.coremetadata/trunk@106362 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/coremetadata/interfaces.py
+++ src/nti/coremetadata/interfaces.py
@@ -48,7 +48,6 @@ from nti.coremetadata import MessageFactory as _
 from nti.schema.field import Bool
 from nti.schema.field import Number
 from nti.schema.field import Object
-from nti.schema.field import ListOrTuple
 from nti.schema.field import ValidTextLine
 from nti.schema.field import ValidDatetime
 from nti.schema.field import UniqueIterable
@@ -531,58 +530,11 @@ class IIdentity(interface.Interface):
     Base interface for Identity base objects
     """
 
-
-class IThreadable(interface.Interface):
-    """
-    Something which can be used in an email-like threaded fashion.
-
-    .. note:: All the objects should be IThreadable, but it is not possible
-            to put that in a constraint without having infinite recursion
-            problems.
-    """
-
-    inReplyTo = Object(interface.Interface,
-                       title="""The object to which this object is directly a reply.""",
-                       required=False)
-
-    references = ListOrTuple(title="""A sequence of objects this object transiently references, in order up to the root""",
-                             value_type=Object(
-                                 interface.Interface, title="A reference"),
-                             default=())
-
-    replies = UniqueIterable(title="All the direct replies of this object",
-                             description="This property will be automatically maintained.",
-                             value_type=Object(interface.Interface, title="A reply"))
-    replies.setTaggedValue('_ext_excluded_out', True)  # Internal use only
-
-    referents = UniqueIterable(title="All the direct and indirect replies to this object",
-                               description="This property will be automatically maintained.",
-                               value_type=Object(interface.Interface, title="A in/direct reply"))
-    referents.setTaggedValue('_ext_excluded_out', True)  # Internal use only
-
-
-class IWeakThreadable(IThreadable):
-    """
-    Just like :class:`IThreadable`, except with the expectation that
-    the items in the reply chain are only weakly referenced and that
-    they are automatically cleaned up (after some time) when deleted. Thus,
-    it is not necessarily clear when a ``None`` value for ``inReplyTo``
-    means the item has never had a reply, or the reply has been deleted.
-    """
-
-
-class IInspectableWeakThreadable(IWeakThreadable):
-    """
-    A weakly threaded object that provides information about its
-    historical participation in a thread.
-    """
-
-    def isOrWasChildInThread():
-        """
-        Return a boolean object indicating if this object is or was
-        ever part of a thread chain. If this returns a true value, it
-        implies that at some point ``inRelpyTo`` was non-None.
-        """
+zope.deferredimport.deprecated(
+    "Import from nti.threadable.interfaces instead",
+    IThreadable='nti.threadable.interfaces:IThreadable',
+    IWeakThreadable='nti.threadable.interfaces:IWeakThreadable',
+    IInspectableWeakThreadable='nti.threadable.interfaces:IInspectableWeakThreadable')
 
 
 class IReadableShared(interface.Interface):
@@ -692,7 +644,6 @@ class IUserGeneratedData(ICreated):
 
 class IModeledContentFile(IFile,
                           INamed,
-                          IThreadable,
                           ILastModified,
                           IShareableModeledContent):
     name = ValidTextLine(title="Identifier for the file",
@@ -703,7 +654,7 @@ IContentFile = IModeledContentFile  # BWC
 # media types
 
 
-class ICanvas(IShareableModeledContent, IThreadable):
+class ICanvas(IShareableModeledContent):
     """
     A drawing or whiteboard that maintains a Z-ordered list of figures/shapes.
     """
@@ -732,7 +683,7 @@ class ICanvasURLShape(ICanvasShape):
     url = interface.Attribute("Shape url")
 
 
-class IMedia(IShareableModeledContent, IThreadable):
+class IMedia(IShareableModeledContent):
     """
     A media object
     """
