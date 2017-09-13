29a8c0bc4a856f93da8451f3d98b73dbfa729942
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-13 04:11:03 +0000

    add isodate to deps; set props; set tox req file; py36 compat
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.coremetadata/trunk@121618 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
     tests_require=TESTS_REQUIRE,
     install_requires=[
         'setuptools',
+        'isodate',
         'nti.base',
         'nti.contentfragments',
         'nti.property',
